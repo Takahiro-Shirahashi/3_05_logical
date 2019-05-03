31c11d97d64b137ffeafa6bdd0faa0116868f0a5
Takahiro Shirahashi <takahiroshirahashi@TakahironoiMac.local>
2019-05-04 03:48:46 +0900

    タイトルとmain.jsの変更

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var hour = new Date().getHours();
 
+if(hour >=0 && hour < 5){
+  window.alert('おやすみなさい');
 if(hour >=19 && hour < 24){
-window.alert('こんばんは');
-}else if(hour === 12){
-window.alert('こんにちは');
-}else if(hour >= 3 && hour < 11){
-window.alert('おはようございます');
+  window.alert('こんばんは');
+}else if(hour >= 12 && hour <= 14){
+  window.alert('こんにちは');
+}else if(hour >= 5 && hour <= 10){
+  window.alert('おはようございます');
+}else{
+  window.alert('お疲れ様です');
 }
 console.log(hour);
