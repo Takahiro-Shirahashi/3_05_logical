37d4868fb721beef850776d912a9220c95adb2e2
Takahiro Shirahashi <takahiroshirahashi@TakahironoiMac.local>
2019-05-05 02:07:16 +0900

    赤文字の追加

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var hour = new Date().getHours();
 
-if(hour >=0 && hour < 5){
+if(hour >= 0 && hour < 5){
   window.alert('おやすみなさい');
-}else if(hour >=19 && hour < 24){
+}else if(hour >= 19 && hour < 24){
   window.alert('こんばんは');
 }else if(hour >= 12 && hour <= 14){
   window.alert('こんにちは');
